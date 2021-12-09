5faeff9e51ffe7e79f7045b80f82ca49f22bbe89
Keith N <78460602+KeithLion@users.noreply.github.com>
2021-12-09 10:41:01 -0600

    added withdraw and deposit method

--- bank_account.py
+++ bank_account.py
@@ -4,4 +4,14 @@ class BankAccount:
         self.intrest = int_rate
         self.account = balance
 
-        
+    def deposit(self,amount):
+        self.account += amount
+        return self
+    
+    def withdraw(self,amount):
+        if(self.account - amount) > 0:
+            self.account -= amount
+        else:
+            print('insufficient funds: charging $5 fee')
+            self.account - 5
+            
