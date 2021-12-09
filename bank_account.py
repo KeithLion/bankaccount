76167795ed88d49e49452003260952609d51864e
Keith N <78460602+KeithLion@users.noreply.github.com>
2021-12-09 10:44:08 -0600

    added display account balance method. and a method that adds interest.

--- bank_account.py
+++ bank_account.py
@@ -7,11 +7,19 @@ class BankAccount:
     def deposit(self,amount):
         self.account += amount
         return self
-    
+
     def withdraw(self,amount):
         if(self.account - amount) > 0:
             self.account -= amount
         else:
             print('insufficient funds: charging $5 fee')
             self.account - 5
-            
+
+    def display_account(self):
+        print(f'Balance:{self.account}')
+
+    def yield_intrest(self):
+        if(self.account > 0):
+            self.account += self.account*0.07
+
+
